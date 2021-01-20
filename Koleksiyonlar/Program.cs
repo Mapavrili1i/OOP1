0f2a58ac3153eef5cc5267d0a14331c74e80801d
Osman <osman-95@hotmail.com>
2021-01-20 23:55:08 +0300

    Proje dosyası ekle.

---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string> { "Osman", "Furkan", "Emre", "Ahmet" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            Console.WriteLine(isimler[3]);
+
+            isimler.Add("Saadet");
+            Console.WriteLine(isimler[4]);
+            Console.WriteLine(isimler[0]);
+        }
+    }
+}
---
+++ OPP1/ProductManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OPP1
+{
+    class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName+" Eklendi.");
+        }
+
+        public void Update(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Güncellendi.");
+        }
+
+        public void Delete(Product product)
+        {
+            Console.WriteLine(product.ProductName + " Silindi.");
+        }
+
+
+
+
+
+    }
+}
